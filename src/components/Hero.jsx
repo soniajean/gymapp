bffe2ba80eb534a1ae5bb2b9aa6b0975234ec127
Sonia <coleysonia@gmail.com>
2023-05-03 20:54:29 -0400

    j

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () = {
+    return (
+        <div className="hero">
+            <div className="left-h">Left Side</div>
+            <div className="right-h">Right Side</div>
+        </div>
+    )
+}
+export default Hero
